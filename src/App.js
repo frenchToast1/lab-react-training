50a10a56d34eff5fe2c5c978cda145b7162ca6c0
frenchToast <juliekogan1@gmail.com>
2022-01-11 19:32:49 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+const IdCard = ({ lastName, firstName, gender, height, birth, picture }) => {
+  return (
+    <div>
+      <img src={picture} alt="identity" />
+      <strong>First name:{firstName}</strong>
+      <strong>Last name:{lastName}</strong>
+      <strong>Gender:{gender}</strong>
+      <strong>Height:{height}</strong>
+      <strong>Birth:{birth}</strong>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <IdCard
+        lastName="Doe"
+        firstName="John"
+        gender="male"
+        height={178}
+        birth={new Date('1992-07-14')}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+      />
+      <IdCard
+        lastName="Delores "
+        firstName="Obrien"
+        gender="female"
+        height={172}
+        birth={new Date('1988-05-11')}
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+      />
     </div>
   );
 }
